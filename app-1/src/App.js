eb6d126d21a4b03c6322ae9f734e493b628ddab1
unknown <sutherlandscott21@gmail.com>
2019-09-26 21:36:54 -0600

    Finished problem

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      text: ""
+    }    
+
+  }
+
+  handleInputText = (value) => {
+    this.setState({
+      text: value
+    })
+  } 
   render() {
+    console.log(this.state)
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* Why is there a type="text"? I don't see what it's doing.  */}
+        <input onChange={(e) => this.handleInputText(e.target.value)} type="text"/>
+        <p>{this.state.text}</p>
       </div>
     );
   }
---
+++ app-1/src/Input.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default class Input extends Component {
+    render() {
+        return(
+            
+        )
+    }
+}
