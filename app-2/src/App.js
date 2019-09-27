e3ac792e397189c3a93aea94d78e85c49ec71db7
unknown <sutherlandscott21@gmail.com>
2019-09-27 08:48:21 -0600

    completed app

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      list: ['learn html', 'learn css', 'learn JS', 'learn React', 'learn React again:)']
+    }
+  }
+
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>{this.state.list[0]}</h2>
+        <h2>{this.state.list[1]}</h2>
+        <h2>{this.state.list[2]}</h2>
+        <h2>{this.state.list[3]}</h2>
+        <h2>{this.state.list[4]}</h2>
+        
       </div>
     );
   }
