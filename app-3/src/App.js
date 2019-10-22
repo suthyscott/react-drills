c94f52e737c3cee9defbd5fd19044d96d4e07e96
unknown <sutherlandscott21@gmail.com>
2019-10-22 14:21:35 -0600

    completed with hooks

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,21 +1,53 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+// import React, { Component } from "react";
+// import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+// class App extends Component {
+//   constructor(){
+//     super()
+//     this.state = {
+//       list: ['Apple', 'Google', 'Samsung', 'Nokia', 'LG']
+//     }
+//   }
+
+//   handleList() {
+    
+//   }
+
+//   render() {
+//     return (
+//       <div className="App">
+      
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+
+import React, {useState, useEffect} from 'react';
+
+const App = () => {
+  const [list, setList] = useState(['pie', 'cake', 'chicken', 'pudding'])
+  const [filteredList, setFilteredList] = useState('')
+
+  let listToFilter = list.filter(element => {
+    return element.includes(filteredList)
+  }).map((e, i) => {
+    return <h2 key={i}>{e}</h2>
+  })
+
+  const filterList = (e) => {
+    setFilteredList(e.target.value)
   }
+
+  return(
+    <div>
+      <input onChange={e => filterList(e)}/>
+
+      {listToFilter}
+    </div>
+  )
 }
 
-export default App;
+export default App
