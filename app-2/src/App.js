9738ddc0330c5fee0ae79a05e4ac1ce94ba5edd2
unknown <sutherlandscott21@gmail.com>
2019-10-22 13:59:44 -0600

    completed with hooks

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,47 +1,65 @@
-import React, { Component } from "react";
-import "./App.css";
-import FirstListItem from './Components/FirstListItem';
-import SecondListItem from './Components/SecondListItem';
-import ThirdListItem from './Components/ThirdListItem';
-import FourthListItem from './Components/FourthListItem';
-import FifthListItem from './Components/FifthListItem';
-import AddListItem from './Components/AddListItem';
+// import React, { Component } from "react";
+// import "./App.css";
+// import FirstListItem from './Components/FirstListItem';
+// import SecondListItem from './Components/SecondListItem';
+// import ThirdListItem from './Components/ThirdListItem';
+// import FourthListItem from './Components/FourthListItem';
+// import FifthListItem from './Components/FifthListItem';
+// import AddListItem from './Components/AddListItem';
 
-class App extends Component {
-  constructor(){
-    super()
+// class App extends Component {
+//   constructor(){
+//     super()
 
-    const list = ['learn html', 'learn css', 'learn JS', 'learn React', 'learn React again:)']
-
-
-    this.state = {
-      list
-    }
-  }
+//     const list = ['learn html', 'learn css', 'learn JS', 'learn React', 'learn React again:)']
 
 
+//     this.state = {
+//       list
+//     }
+//   }
 
-handleInput = (val) => {
-  let copy = [...this.state.list]
-  copy[4] = val
+
+
+// handleInput = (val) => {
+//   let copy = [...this.state.list]
+//   copy[4] = val
   
-  this.setState({
-    list: copy
-  })
+//   this.setState({
+//     list: copy
+//   })
+// }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <FirstListItem item1={this.state.list[0]}/>
+//         <SecondListItem item2={this.state.list[1]}/>
+//         <ThirdListItem item3={this.state.list[2]}/>
+//         <FourthListItem item4={this.state.list[3]}/>
+//         <FifthListItem item5={this.state.list[4]}/>  
+//         <AddListItem handleInput={this.handleInput}/>      
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+
+import React, {useState, useEffect} from 'react';
+
+const App = () => {
+  const [list, setList] = useState(['hello', 'hi', 'greetings'])
+
+  return(
+    <div>
+      <h2>{list[0]}</h2>
+      <h2>{list[1]}</h2>
+      <h2>{list[2]}</h2>
+      
+    </div>
+  )
 }
 
-  render() {
-    return (
-      <div className="App">
-        <FirstListItem item1={this.state.list[0]}/>
-        <SecondListItem item2={this.state.list[1]}/>
-        <ThirdListItem item3={this.state.list[2]}/>
-        <FourthListItem item4={this.state.list[3]}/>
-        <FifthListItem item5={this.state.list[4]}/>  
-        <AddListItem handleInput={this.handleInput}/>      
-      </div>
-    );
-  }
-}
-
-export default App;
+export default App
